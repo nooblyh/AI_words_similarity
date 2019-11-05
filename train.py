fbc900f8dd0511b1f37113914d66790e9c364615
nooblyh <472897494@qq.com>
2019-11-05 22:41:07 +0800

    revert

--- train.py
+++ train.py
@@ -1,11 +1,8 @@
-import gensim
 from get_data import get_data
 from gensim.models import Word2Vec
 import time
 localtime = time.asctime( time.localtime(time.time()))
 text = get_data()
-model = gensim.models.KeyedVectors.load_word2vec_format('modelfile/GoogleNews-vectors-negative300.bin.gz', binary=True)
-model.build_vocab(sentences=text, update=True)
-model.train(sentences=text, total_examples=model.corpus_count, epochs=model.iter)
+model = Word2Vec(text, size=100, window=5, min_count=1, workers=4)
 model.save('./modelfile/Model%s'%localtime)
 model.wv.save_word2vec_format('./modelfile/model%s.txt'%localtime, binary=False)
