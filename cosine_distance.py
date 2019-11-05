fd2911a879edbf7d9be1b749e4ce635ce4ddc4d9
nooblyh <472897494@qq.com>
2019-11-05 11:20:34 +0800

    fix#3

--- cosine_distance.py
+++ cosine_distance.py
@@ -3,7 +3,7 @@ def cosine_similarity(x, y, norm=False):
     assert len(x) == len(y), "len(x) != len(y)"
     zero_list = [0] * len(x)
     if (x - zero_list).any() or (y - zero_list).any():
-        return float(1) if x == y else float(0)
+        return float(1) if (x - y).any() else float(0)
     res = np.array([[x[i] * y[i], x[i] * x[i], y[i] * y[i]] for i in range(len(x))])
     cos = sum(res[:, 0]) / (np.sqrt(sum(res[:, 1])) * np.sqrt(sum(res[:, 2])))
     return 0.5 * cos + 0.5 if norm else cos  # 归一化到[0, 1]区间内
