47f2d9b4a2dbeef6db62bf7082f5677983d05a95
nooblyh <472897494@qq.com>
2019-11-05 11:18:05 +0800

    fix#2

--- cosine_distance.py
+++ cosine_distance.py
@@ -1,14 +1,8 @@
 import numpy as np
-
-
-def bit_product_sum(x, y):
-    return sum([item[0] * item[1] for item in zip(x, y)])
-
-
 def cosine_similarity(x, y, norm=False):
     assert len(x) == len(y), "len(x) != len(y)"
     zero_list = [0] * len(x)
-    if x == zero_list or y == zero_list:
+    if (x - zero_list).any() or (y - zero_list).any():
         return float(1) if x == y else float(0)
     res = np.array([[x[i] * y[i], x[i] * x[i], y[i] * y[i]] for i in range(len(x))])
     cos = sum(res[:, 0]) / (np.sqrt(sum(res[:, 1])) * np.sqrt(sum(res[:, 2])))
