29dd0a004492d000d0d493f68f8936e3d10a71b5
nooblyh <472897494@qq.com>
2019-11-05 11:07:28 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from get_data import get_data
 from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
+from cosine_distance import cosine_similarity;
 
 text = get_data()
 model = Word2Vec(text, size=100, window=5, min_count=1, workers=4)
