16630d7204b18e1c83e50327ea9e3122c2c87934
nooblyh <472897494@qq.com>
2019-11-07 11:40:09 +0800

    creating new word map

---
+++ create_new_map.py
@@ -0,0 +1,17 @@
+from gensim.models import Word2Vec
+
+model = Word2Vec.load('./modelfile/MyModel')
+with open("spec.txt","r") as dict_file:
+    with open("words.txt","r") as words_file:
+        words = words_file.read().splitlines()
+        for w in words:
+            print(w+":")
+            for d_w in dict_file:
+                if("/" in d_w):
+                    index = d_w.index()
+                    if model.wmdistance(d_w[0,index],w) < 5 or model.wmdistance(d_w[index+1:],w) < 5:
+                        print(d_w[0,index])
+                else:
+                    if model.wmdistance(d_w,w) < 5:
+                        print(d_w)
+
