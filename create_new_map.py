ad40d553a662823eb3af74cd11b89a2466fa24f6
nooblyh <472897494@qq.com>
2019-11-07 12:15:23 +0800

    fix#5

--- create_new_map.py
+++ create_new_map.py
@@ -12,7 +12,7 @@ with open("dict.txt","r") as dict_file:
                 if("/" in d_w):
                     index = d_w.index("/")
                     if model.wmdistance(d_w[0:index],w) < threshold or model.wmdistance(d_w[index+1:],w) < threshold:
-                        print(d_w[0,index])
+                        print(d_w[0:index])
                 else:
                     if model.wmdistance(d_w,w) < threshold:
                         print(d_w)
