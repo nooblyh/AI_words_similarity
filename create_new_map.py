a38fa7d8fb06831e93b7bf62efaab502c9c70731
nooblyh <472897494@qq.com>
2019-11-07 12:11:56 +0800

    fix#4

--- create_new_map.py
+++ create_new_map.py
@@ -5,9 +5,10 @@ model = Word2Vec.load('./modelfile/MyModel')
 with open("dict.txt","r") as dict_file:
     with open("words.txt","r") as words_file:
         words = words_file.read().splitlines()
+        dict_words = dict_file.read().splitlines()
         for w in words:
             print(w+":")
-            for d_w in dict_file:
+            for d_w in dict_words:
                 if("/" in d_w):
                     index = d_w.index("/")
                     if model.wmdistance(d_w[0:index],w) < threshold or model.wmdistance(d_w[index+1:],w) < threshold:
