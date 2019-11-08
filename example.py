2d4332af55f4d3964ea8ca581983e726ca8d2848
nooblyh <472897494@qq.com>
2019-11-08 10:22:26 +0800

    fix logic bug

--- example.py
+++ example.py
@@ -22,13 +22,13 @@ with open("dict.txt","r") as dict_file:
                 t_w_origin = [i for i in t_w_origin if i not in stopwords]
                 t_w_abbr = t_w[index+1:].lower().split()
                 t_w_abbr = [i for i in t_w_abbr if i not in stopwords]
-                if model.wmdistance(t_w_origin,w) < threshold:
-                    break
-                elif model.wmdistance(t_w_abbr,w) < threshold:
+                if model.wmdistance(t_w_origin,w) == threshold:
+                    continue
+                elif model.wmdistance(t_w_abbr,w) == threshold:
                     w = t_w_origin
                     tmp.append(w)
-                else:
-                    continue
+                elif model.wmdistance(t_w_origin,w) < threshold or model.wmdistance(t_w_abbr,w) < threshold:
+                    tmp.append(t_w_origin)
                     
         
             for d_w in dict_words:
