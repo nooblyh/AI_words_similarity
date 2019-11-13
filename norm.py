7efa97b1c979b2f964aa957b59c055b10769c588
nooblyh <472897494@qq.com>
2019-11-13 17:39:59 +0800

    fix

--- norm.py
+++ norm.py
@@ -7,14 +7,20 @@ threshold = 2.0
 with open("words.txt","r") as words_file:
     words = words_file.read().splitlines()
 
-for i in range(0,len(words)):
-    for j in range(i+1,len(words)):
-        disctance = model.wmdistance(words[i],words[j])
+i=0
+
+for w1 in words:
+    j=i+1
+    while j<len(words):
+        w2 = words[j]
+        disctance = model.wmdistance(w1,w2)
         if threshold > disctance:
-            print(words[i]+"\t"+words[j])
+            print(w1+"\t"+w2)
             print(disctance)
             words.pop(j)
             j=j-1
+        j=j+1
+    i=i+1
     
 to = open("new_words.txt",'w',encoding="UTF-8")
 for w in words:
