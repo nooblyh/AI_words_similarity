b725f88cdd05eb55a94a3000e710ac4a859611c3
nooblyh <472897494@qq.com>
2019-11-13 19:06:04 +0800

    词形还原

--- norm.py
+++ norm.py
@@ -1,27 +1,37 @@
 from load_words import find_similar
 from gensim.models import Word2Vec
+from nltk.stem import WordNetLemmatizer
 
-model = Word2Vec.load('./modelfile/MyModel')
-threshold = 2.0
+def same_words(lw1,lw2):
+    for i in range(0,len(lw1)):
+        if wnl.lemmatize(lw1[i]) != wnl.lemmatize(lw2[i]):
+            return False
+    return True
 
-with open("words.txt","r") as words_file:
-    words = words_file.read().splitlines()
 
-i=0
+if __name__ == "__main__":
+    wnl = WordNetLemmatizer()
+    model = Word2Vec.load('./modelfile/MyModel')
+    threshold = 2.0
 
-for w1 in words:
-    j=i+1
-    while j<len(words):
-        w2 = words[j]
-        disctance = model.wmdistance(w1,w2)
-        if threshold > disctance:
-            print(w1+"\t"+w2)
-            print(disctance)
-            words.pop(j)
-            j=j-1
-        j=j+1
-    i=i+1
-    
-to = open("new_words.txt",'w',encoding="UTF-8")
-for w in words:
-    to.write(w+"\n")
+    with open("words.txt","r") as words_file:
+        words = words_file.read().splitlines()
+
+    i=0
+
+    for w1 in words:
+        j=i+1
+        while j<len(words):
+            w2 = words[j]
+            lw1 = w1.split()
+            lw2 = w2.split()
+            if len(lw1) == len(lw2) and same_words(lw1,lw2):
+                print(w1+"\t"+w2)
+                words.pop(j)
+                j=j-1
+            j=j+1
+        i=i+1
+        
+    to = open("new_words.txt",'w',encoding="UTF-8")
+    for w in words:
+        to.write(w+"\n")
