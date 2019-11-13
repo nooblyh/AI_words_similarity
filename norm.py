027a1b2422ef11c0ea7022f2ff4f13ec709370ed
nooblyh <472897494@qq.com>
2019-11-13 17:24:02 +0800

    fix

--- norm.py
+++ norm.py
@@ -8,10 +8,11 @@ with open("words.txt","r") as words_file:
     words = words_file.read().splitlines()
 
 for i in range(0,len(words)):
+    print(words[i])
     for j in range(i+1,len(words)):
         disctance = model.wmdistance(words[i],words[j])
-        if threshold > disctance:
-            print(words[i]+'\t'+words[j])
-            print(disctance)
+        if threshold < disctance:
+            print(words[j])
+        else:
             words.pop(j)
             j=j-1
