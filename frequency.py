b1a4c08ee91555c7bd1275cebf88f085a04b1406
nooblyh <472897494@qq.com>
2019-11-14 10:38:48 +0800

    fix

--- frequency.py
+++ frequency.py
@@ -1,12 +1,16 @@
 from get_data import get_data
 
+sentences = []
 text = get_data()
+for l in text:
+    sentence = " ".join(str(w) for w in l)
+    sentences.append(sentence)
+print("开始统计词频")
 with open("words.txt","r") as words_file:
     words = words_file.read().splitlines()
 for word in words:
     count = 0
-    for s in text:
-        sentence = " ".join(str(w) for w in s)
-        if word in sentence:
+    for s in sentences:
+        if word in s:
             count += 1
     print("\""+word+"\","+count)
