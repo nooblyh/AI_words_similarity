07b4159f3ed58119e74d1ac2997d9840f7e14f32
nooblyh <472897494@qq.com>
2019-12-03 17:29:26 +0800

    fix

--- replace_wrong_word.py
+++ replace_wrong_word.py
@@ -10,20 +10,27 @@ to = open("new_words_except_wrong.txt",'w',encoding="UTF-8")
 with open("words.txt","r") as words_file:
     words = words_file.read().splitlines()
 for word in words:
-    new = []
+    flag = True 
     word = word.split()
     for i in word:
-        true = i
-        if(d.check(true)):
-            new.append(true)
-        else:
-            for s,_ in model.wv.most_similar(i,topn=5):
-                if d.check(s):
-                    true = s
-                    print("INFO!"+ i + "改为" + true)
-                    break
-            new.append(true)
-            if(true == i):
-                print("WARNING!"+ true + "的同义词也是错词")
-    to.write(" ".join(str(i) for i in new)+"\n")
+        if i not in model:
+            flag = False
+            break
+
+    if flag:
+        new = []
+        for i in word:
+            true = i
+            if(d.check(true)):
+                new.append(true)
+            else:
+                for s,_ in model.wv.most_similar(i,topn=5):
+                    if d.check(s):
+                        true = s
+                        print("INFO!"+ i + "改为" + true)
+                        break
+                new.append(true)
+                if(true == i):
+                    print("WARNING!"+ true + "的同义词也是错词")
+        to.write(" ".join(str(i) for i in new)+"\n")
 to.close()
