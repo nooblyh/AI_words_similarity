4122459330b4437a6c2641f3d34bf3c079f55f32
nooblyh <472897494@qq.com>
2019-12-04 10:56:18 +0800

    新subset

--- frequency.py
+++ frequency.py
@@ -1,6 +1,12 @@
 from get_data import get_data
 
-def is_sublist(sublst, lst): return sum([lst == sublst[i: i + len(lst)] for i in range(len(sublst) - len(lst))]) > 0
+def is_subset(s,w):
+    if w not in s:
+        return False
+    if s.index(w) + len(w) == len(s) or s[s.index(w) + len(w)] == " ":
+        return True
+    else:
+        return False
 
 sentences = []
 text = get_data()
@@ -9,9 +15,8 @@ for l in text:
 with open("words.txt","r") as words_file:
     words = words_file.read().splitlines()
 for word in words:
-    word = word.split()
     count = 0
     for s in sentences:
-        if is_sublist(s,word):
+        if is_subset(" ".join(str(i) for i in s),word):
             count += 1
-    print("\"" + " ".join(str(i) for i in word) + "\"," + str(count))
+    print("\"" + word + "\"," + str(count))
