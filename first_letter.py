a6586397f94c3ac89d6139fc23d8eed583a0b6db
nooblyh <472897494@qq.com>
2019-12-18 21:02:10 +0800

    不打印

--- first_letter.py
+++ first_letter.py
@@ -12,19 +12,14 @@ with open("words_v2.txt","r") as words_file:
                 for x in w2.split():
                     head = head + x[0]
                 result[i].append(Levenshtein.seqratio(w1,head))
-                if(Levenshtein.seqratio(w1,head)>0.8):
-                    print(w1,w2)
             elif (len(w1.split())>1 and len(w2.split())==1):
                 head = ""
                 for x in w1.split():
                     head = head + x[0]
                 result[i].append(Levenshtein.seqratio(head,w2))
-                if(Levenshtein.seqratio(head,w2)>0.8):
-                    print(w1,w2)
             else:
                 result[i].append(0)
         i+=1
-print(result)
 with open("matrix.txt","w") as f:
     for i in range(len(result)):
         for j in range(len(result)):
